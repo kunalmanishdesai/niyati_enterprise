080b037d80426a24ebe566837cbb96bed5713ec5
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-07-27 23:25:00 +0530

    Added homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Carousel from "components/carousel";
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <>
+            <Carousel/>
+        </>
+    );
+}
+
+export default HomePage;
