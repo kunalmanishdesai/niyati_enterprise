d96de8d76db9348505edbe886f93b3b8bd9ff7e6
kunalmanishdesai <kunalmanishdesai@gmail.com>
2024-07-29 17:24:42 +0530

    Created product page

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import ImageViewer from "components/ImageViewer";
+import React from "react";
+
+const ProductPage = () => {
+    return(
+        <>
+        <ImageViewer/>
+        </>
+    );
+}
+
+export default ProductPage;
